feef8332625fe09b310a5cefd20d9158b677727b
GOZU Kenichiro <gozuk16@gmail.com>
2021-08-22 13:11:33 +0900

    エラー出力

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/shirou/gopsutil/v3/host"
 )
@@ -9,7 +10,11 @@ import (
 func main() {
 	h1, h2, h3, _ := host.PlatformInformation()
 	fmt.Println(h1, h2, h3)
-	t, _ := host.SensorsTemperatures()
+	t, err := host.SensorsTemperatures()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 	for _, v := range t {
 		fmt.Printf("sensorKey: %s, temperature: %2.0f, sensorHigh: %2.0f, sensorCritical: %2.0f\n", v.SensorKey, v.Temperature, v.High, v.Critical)
 	}
